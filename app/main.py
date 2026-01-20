62e16e7ff6316ad08f8c0152ad01a10d6e0bc9a4
Kushagra Gupta <kushagra7503@gmail.com>
2026-01-20 18:54:50 +0530

    Comments added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="LMS Backend")
+app = FastAPI(title="LMS Backend") # Object of fastAPI class
 
-@app.get("/hello_world")
+@app.get("/hello_world") # decorator - A function that wraps another function and adds behavior to it.
 def hello_world():
     return {"message": "Hello World"}
 

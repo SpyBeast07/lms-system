c8cc9a0ca09fad36d517e790ab3b8fc739f71ce4
Kushagra Gupta <kushagra7503@gmail.com>
2026-03-10 23:11:23 +0530

    feat: Configure Caddy to listen on port 80, remove Node.js types from `tsconfig.node.json`, and add allowed hosts for the Vite preview server.

--- lms-FE/vite.config.ts
+++ lms-FE/vite.config.ts
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['lms.kushagragupta.co.in']
+  },
   build: {
     chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
-        manualChunks(id) {
+        manualChunks(id: string) {
           if (id.includes('node_modules')) {
             return 'vendor';
           }
